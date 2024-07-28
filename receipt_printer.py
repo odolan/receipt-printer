99275f23fdac93c1f5a39bbf0414eb8cc6f585e3
Owen Dolan <41007430+odolan@users.noreply.github.com>
2024-07-28 15:06:57 -0400

    Update receipt_printer.py

--- receipt_printer.py
+++ receipt_printer.py
@@ -1,1 +1,20 @@
-from subprocess import Popenimport subprocessimport osimport io#prints out inputed textdef print_text(text):   correct_printer = 'EPSON_TM_T20II'   printerControl = os.popen('lpr -P %s' % (correct_printer), 'w')   printerControl.write(text)   printerControl.close()#prints out inputed imagedef print_image(img_path):   #connect and print image on printer   with open(img_path) as f:       # call the system's lpr command       correct_printer = 'EPSON_TM_T20II'       Popen(['lpr -P %s' % (correct_printer)], stdin=f, shell=True)  # not sure you need shell=True for a simple command
+from subprocess import Popen
+import subprocess
+import os
+import io
+
+
+#prints out inputed text
+def print_text(text):
+   correct_printer = 'EPSON_TM_T20II'
+   printerControl = os.popen('lpr -P %s' % (correct_printer), 'w')
+   printerControl.write(text)
+   printerControl.close()
+
+#prints out inputed image
+def print_image(img_path):
+   #connect and print image on printer
+   with open(img_path) as f:
+       # call the system's lpr command
+       correct_printer = 'EPSON_TM_T20II'
+       Popen(['lpr -P %s' % (correct_printer)], stdin=f, shell=True)  # not sure you need shell=True for a simple command
